c69dace94d5a786b31bddf464d8ae601d62bcb42
przemek <pwenderholm@gmail.com>
2023-02-26 19:21:44 +0100

    data 1

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,7 @@ public class Main {
         System.out.println("Hello world!" + a);
         System.out.println("Hello world!" + a);
         System.out.println("Hello world!" + a);
+
+        System.out.println("dodano test");
     }
 }
