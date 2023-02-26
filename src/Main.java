fbfc27c45f5fa8fa420312c8abd076bee4385cbb
“przemek <pwenderholm@gmail.com>
2023-02-26 21:49:07 +0100

    zmiana w main

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,7 @@ public class Main {
         System.out.println("Hello world!" + a);
 
         System.out.println("dodano test");
+        Dog dog = new Dog("burek");
+        System.out.println(dog.getName());
     }
 }
